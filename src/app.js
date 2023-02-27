e4ef39917c3f6c3c3eabe911d233b38fbdee5ebe
jaime <jbenaci736@g.educaaand.es>
2023-02-27 13:25:52 +0100

    Primera parte con carruseles terminada

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 const grande = document.querySelector('.grande')
 const punto = document.querySelectorAll('.punto')
+const grandeHistoria = document.querySelector('.grande-historia')
+const puntoHistoria = document.querySelectorAll('.punto-historia')
 
 punto.forEach( (cadaPunto, i) => {
     //asignamos click a cada punto
@@ -17,5 +19,22 @@ punto.forEach( (cadaPunto, i) => {
             punto[i].classList.remove('activo')
         })
         punto[i].classList.add('activo')
+    })
+})
+
+puntoHistoria.forEach( (cadaPunto, i) => {
+    //asignamos click a cada punto
+    puntoHistoria[i].addEventListener('click', () =>{
+        //posicion de cada punto en el array
+        let posicion = i
+        //calculo de la posicion de los videos segun el punto
+        let operacion = posicion * -25
+
+        grandeHistoria.style.transform = `translateX(${operacion}%)`
+        //cambios de clase
+        puntoHistoria.forEach( ( cadaPunto, i) =>{
+            puntoHistoria[i].classList.remove('activo')
+        })
+        puntoHistoria[i].classList.add('activo')
     })
 })
